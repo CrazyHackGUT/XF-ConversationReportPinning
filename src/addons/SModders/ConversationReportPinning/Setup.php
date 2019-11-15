0cd7410a0f12c8974f6f4462603821de1c0b94f9
Kruzya <kruzefag@gmail.com>
2019-11-15 19:59:11 +0400

    Initial commit

---
+++ src/addons/SModders/ConversationReportPinning/Setup.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is a part of [Report Improvements] Conversation Pinning.
+ * All rights reserved.
+ *
+ * Developed by SourceModders.
+ */
+
+namespace SModders\ConversationReportPinning;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+}
